e3178c3f5cb3207a83abdcde278671def57efd8f
RJ-Csc <rolan_cosico@dlsu.edu.ph>
2022-06-07 21:36:37 +0800

    Update [jobId].js

--- pages/j/[jobId].js
+++ pages/j/[jobId].js
@@ -37,10 +37,9 @@ export default function JobIDPage() {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Download</button>
         </a> : <></>;
     
-    let videoPreview = data.link ?
-        <iframe className='aspect-auto'>
-            <source src={data.link} type="video/mp4"/>
-        </iframe> : <></>;
+    let videoPreview =  <video width="600" height="400" controls>
+                            <source src={data.link} type="video/mp4"></source>
+                        </video>
 
 
     return (
@@ -53,18 +52,19 @@ export default function JobIDPage() {
                 </div>
                 
                 <div class="bg-green-400 w-1/2 h-full flex justify-center">
-                    <h1> 2D </h1>
+                    <h1> 3D </h1>
                     {videoPreview}
                 </div>
             </div>
 
-            {data.progress}
+            <div class="w-2/3 bg-gray-300 rounded-full my-2">
+                <div class="bg-gray-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: data.progress + "%"}}> {data.progress}%</div>
+            </div>
             
             <p>Status: {data.status}</p>
             <p>Message: {data.message}</p>
             <p>Trajectory: {data.traj}</p>
             {downloadButton}
-            {videoPreview}
         </div>
     )
 }
