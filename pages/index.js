27d82d34cb6dbe9acd4256e14aed571294607216
Aaron Brines <aaron_brines@dlsu.edu.ph>
2022-06-07 19:55:02 +0800

    Front End changes

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import Banner from '../components/Banner';
 
+
 export default function Home() {
 
   const router = useRouter();
@@ -44,11 +45,25 @@ export default function Home() {
     }
   }
 
+  let videolink = "moon_circle.mp4"
+
+  switch(traj){
+    case "circle": videolink = "../moon_circle.mp4"; break;
+    case "swing": videolink = "../moon_swing.mp4"; break;
+    case "zoom-in": videolink = "../moon_zoom-in.mp4"; break;
+    case "dolly-zoom-in": videolink = "../zoom_dolly-zoom-in"; break;
+  }
+
+  let videotag = <video width="600" height="400" controls>
+                    <source src={videolink} type="video/mp4"></source>
+                </video>
+
+
   return (
     <div>
       <Banner/>
       <div className='grid place-items-center my-2'>
-        <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="file_input">Upload file</label>
+        <label className="block mb-2 text-m font-bold text-gray-900" htmlFor="file_input">Convert image (.jpg) to 3D Photo</label>
         <input type="file" 
           accept=".jpg"
           onChange={handleChange}
@@ -59,33 +74,42 @@ export default function Home() {
           file:bg-blue-500 file:text-white
           hover:file:bg-blue-700
         "/>
+          <div class="flex w-5/6 justify-between m-4">
+            <div>
+              <input type="radio" id="swing" name="traj" value="swing" onChange={handleTrajChange} checked={traj=="swing"}/>
+              <label htmlFor="swing">Swing</label>
+            </div>
 
-        <div>
-          <input type="radio" id="swing" name="traj" value="swing" onChange={handleTrajChange} checked={traj=="swing"}/>
-          <label htmlFor="swing">Swing</label>
+            <div>
+              <input type="radio" id="circle" name="traj" value="circle" onChange={handleTrajChange} checked={traj=="circle"}/>
+              <label htmlFor="circle">Circle</label>
+            </div>
+
+            <div>
+              <input type="radio" id="zoom" name="traj" value="zoom-in" checked={traj=="zoom-in"} onChange={handleTrajChange}/>
+              <label htmlFor="zoom">Zoom</label>
+            </div>
+
+            <div>
+              <input type="radio" id="dolly-zoom" name="traj" value="dolly-zoom-in" checked={traj=="dolly-zoom-in"} onChange={handleTrajChange}/>
+              <label htmlFor="dolly-zoom">Dolly Zoom</label>
+            </div>
         </div>
 
-        <div>
-          <input type="radio" id="circle" name="traj" value="circle" onChange={handleTrajChange} checked={traj=="circle"}/>
-          <label htmlFor="circle">Circle</label>
+        <div class="font-medium">
+          <label >Preview</label>
         </div>
 
-        <div>
-          <input type="radio" id="zoom" name="traj" value="zoom-in" checked={traj=="zoom-in"} onChange={handleTrajChange}/>
-          <label htmlFor="zoom">Zoom</label>
-        </div>
-
-        <div>
-          <input type="radio" id="dolly-zoom" name="traj" value="dolly-zoom-in" checked={traj=="dolly-zoom-in"} onChange={handleTrajChange}/>
-          <label htmlFor="dolly-zoom">Dolly Zoom</label>
+        <div class="flex w-5/6 justify-center m-4">
+          {videotag}
         </div>
         
         <button 
           onClick={handleSubmit}
           className="bg-blue-500 hover:bg-blue-700
-        text-white font-bold py-2 px-4 rounded">Submit</button>
-        <div class="w-full bg-gray-200 rounded-full">
-          <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: progress + "%"}}> {progress}%</div>
+        text-white font-bold py-2 px-4 rounded m-4">Submit</button>
+        <div class="w-2/3 bg-gray-200 rounded-full">
+          <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: progress + "%"}}> {progress}%</div>
         </div>
       </div>
     </div>
